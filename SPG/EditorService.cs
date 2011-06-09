1f9c5df10e63f97a689c3f4249eb52aca45f54fb
Denis Vuyka <denis.vuyka@gmail.com>
2011-06-09 16:41:13 +0300

    Extended EditorService to allow registering global editors for property types

--- SPG/EditorService.cs
+++ SPG/EditorService.cs
@@ -17,11 +17,38 @@
 using System.ComponentModel;
 using System.Windows.Controls.PropertyGrid.PropertyEditing;
 using System.Windows.Media;
+using System.Collections.Generic;
 
 namespace System.Windows.Controls.PropertyGrid
 {
   public static class EditorService
   {
+    private static readonly Dictionary<Type, Type> _registeredEditors = new Dictionary<Type, Type>();
+
+    public static void RegisterEditor(Type propertyType, Type editorType)
+    {
+      Requires.NotNull(propertyType, "propertyType");
+      Requires.NotNull(editorType, "editorType");
+
+      if (!typeof(EditorBase).IsAssignableFrom(editorType))
+        throw new ArgumentException("editorType should inherit EditorBase type");
+
+      _registeredEditors[propertyType] = editorType;
+    }
+
+    public static EditorBase GetRegisteredEditor(PropertyLabel label, PropertyItem propertyItem)
+    {
+      Type editorType = null;
+      if (!_registeredEditors.TryGetValue(propertyItem.PropertyType, out editorType)) return null;
+      var instance = Activator.CreateInstance(editorType, label, propertyItem);
+      return instance as EditorBase;
+    }
+
+    public static void RegisterEditor<TPropertyType, TEditorType>() where TEditorType : EditorBase
+    {
+      _registeredEditors[typeof(TPropertyType)] = typeof(TEditorType);
+    }
+
     public static EditorBase GetEditor(PropertyItem propertyItem, PropertyLabel label)
     {
       if (propertyItem == null) throw new ArgumentNullException("propertyItem");
@@ -33,6 +60,9 @@ namespace System.Windows.Controls.PropertyGrid
         if (editorType != null)
           return Activator.CreateInstance(editorType, label, propertyItem) as EditorBase;
       }
+
+      var registeredEditor = GetRegisteredEditor(label, propertyItem);
+      if (registeredEditor != null) return registeredEditor;
 
       Type propertyType = propertyItem.PropertyType;
 
